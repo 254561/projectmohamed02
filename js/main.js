5380f69269880c39386fb0ce63f0b8e4726419cc
mohamedreda1258 <mohamed reda.com>
2023-07-14 23:02:57 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel();
+  });
+  /*--------------------------------*/
+  
+	$(".m").counterUp({
+    delay:5,
+    time:1500
+});
+$(".mm").counterUp({
+    delay:5,
+    time:1500
+});
+
+
+$(".mmm").counterUp({
+    delay:5,
+    time:1500
+});
+
+
+$(".mmmm").counterUp({
+    delay:5,
+    time:1500
+});
+
+
+
+$(".h").counterUp({
+    delay:5,
+    time:1500
+});
+$(".hh").counterUp({
+    delay:5,
+    time:1500
+});
+
+
+$(".hhh").counterUp({
+    delay:5,
+    time:1500
+});
+
+
+
+
+
+
+
+
+
+
+
